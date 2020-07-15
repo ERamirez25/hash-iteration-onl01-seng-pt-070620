87bbefba855663afacbac1eb50666c7f182d3d80
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-15 20:52:59 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,6 +14,7 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, kids_age|
     if kids_age <= 12 
     puts "Happy Bithday #{name}! You are now #{kids_age} year old!"
+  else 
   end 
 end 
 end 
